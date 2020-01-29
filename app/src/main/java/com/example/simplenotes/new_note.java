249803727d431e451b520f57df132aa23c880760
Stephen Odundo <steveremdi@gmail.com>
2020-01-29 21:03:52 +0300

    The new note view fetches text and places it in the appropriate structures.

--- app/src/main/java/com/example/simplenotes/new_note.java
+++ app/src/main/java/com/example/simplenotes/new_note.java
@@ -1,16 +1,18 @@
 package com.example.simplenotes;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.material.snackbar.Snackbar;
 
 public class new_note extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,24 @@ public class new_note extends AppCompatActivity {
         setContentView(R.layout.activity_new_note);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final NotesHandler newlyCreatedNotes = new NotesHandler();
+
+        final EditText noteTitleFromView = findViewById(R.id.new_note_title);
+        final EditText noteContentFromView = findViewById(R.id.new_note_content);
+
+
+        Button saveNoteFromView = findViewById(R.id.save_note_button);
+        saveNoteFromView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Note newNote = new Note(noteTitleFromView.getText().toString(), noteContentFromView.getText().toString(), false);
+                newlyCreatedNotes.addNoteToList(newNote);
+
+                Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinatorLayout), "Saved note.", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        });
     }
 
 }
